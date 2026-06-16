238056cf429d5c7a39eef4faa8870c9953084098
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-06-16 09:16:00 +0200

    exercises katas python 28

--- katas-python/ejercicio-28.py
+++ katas-python/ejercicio-28.py
@@ -1,5 +1,15 @@
 #Crea una función que busque y devuelva el 
 # primer elemento duplicado en una lista dada.
 
-lista_numeros = [2,6,7,5,2]
+lista_numeros = [2,6,7,5,2,6,6]
+repetidos = []
 
+def find_repetidos(lista):
+
+    for numero in lista:
+        if lista.count(numero) > 1 and numero not in repetidos:
+            repetidos.append(numero)
+            return repetidos
+
+resultado = find_repetidos(lista_numeros)
+print(resultado)
